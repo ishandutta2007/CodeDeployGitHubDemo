6c0d5b801ead39747f234c9719a4de5d06d957b0
ishan dutta <ishandutta2007@gmail.com>
2017-03-13 08:28:23 +0530

    Create afterInstall.sh

---
+++ scripts/afterInstall.sh
@@ -0,0 +1,1 @@
+rsync --delete-before --verbose --archive --exclude "htdocs/media/" --exclude ".*" --exclude "htdocs/var/" --exclude "htdocs/app/etc/local.xml" /var/www/release/ /var/www/magento/ > /var/log/deploy.log
