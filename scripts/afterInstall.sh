ce65aeb1640d5c8413e5f161318ffd800953f370
ishan dutta <ishandutta2007@gmail.com>
2017-03-13 08:28:52 +0530

    Update afterInstall.sh

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 rsync --delete-before --verbose --archive --exclude "htdocs/media/" --exclude ".*" --exclude "htdocs/var/" --exclude "htdocs/app/etc/local.xml" /var/www/release/ /var/www/magento/ > /var/log/deploy.log
