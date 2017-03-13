f69705d0bc262f42a8248b18027e07ecc6b29f23
ishan dutta <ishandutta2007@gmail.com>
2017-03-13 08:31:02 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,10 +4,10 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>/home/ubuntu/out.log 2>&1
 
-if [ -d /var/www/release ]; then
-    rm -rf /var/www/release
+if [ -d /var/www/html ]; then
+    rm -rf /var/www/html
 fi
-mkdir -vp /var/www/release
+mkdir -vp /var/www/html
 
 apt-get install -y apache2
 
